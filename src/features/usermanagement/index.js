2b7c9111fbd75531098df0cb61afb0eecee93ec5
sovortey <126035238+sovortey514@users.noreply.github.com>
2024-09-05 13:08:26 +0700

    update

---
+++ src/features/usermanagement/index.js
@@ -0,0 +1,17 @@
+
+import React, { useState } from 'react';
+
+
+
+function ToatalUser() {
+ 
+  return (
+    <>
+      <div className='mt-12'>
+        Hello history of asset count
+      </div>
+    </>
+  );
+}
+
+export default ToatalUser;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,6 @@
 
 import { User } from '@nextui-org/react'
 import { lazy } from 'react'
-// import UserManagement from '../pages/protected/usermanagement';
-
 const Dashboard = lazy(() => import('../pages/protected/Dashboard'))
 const Welcome = lazy(() => import('../pages/protected/Welcome'))
 const Page404 = lazy(() => import('../pages/protected/404'))
@@ -11,23 +9,27 @@ const Blank = lazy(() => import('../pages/protected/Blank'))
 const Charts = lazy(() => import('../pages/protected/Charts'))
 const Leads = lazy(() => import('../pages/protected/Leads'))
 const Integration = lazy(() => import('../pages/protected/Integration'))
-// const Calendar = lazy(() => import('../pages/protected/Calendar'))
 const Team = lazy(() => import('../pages/protected/Team'))
 const GettingStarted = lazy(() => import('../pages/GettingStarted'))
-
 const TotalAsset = lazy(() => import('../pages/protected/TotalAsset'))
 const AssetCount = lazy(() => import('../pages/protected/AssetCount'))
 const HistoryCount = lazy(() => import('../pages/protected/HistoryCount'))
 const ProfileSettings = lazy(() => import('../pages/protected/ProfileSettings'))
-// const Users = lazy(() => import('../pages/protected/Users'))
+
+const ManageUser = lazy(() => import('../pages/protected/ManageUser'))
+
 const routes = [
   {
-    path: '/dashboard', // the url
-    component: Dashboard, // view rendered
+    path: '/dashboard', 
+    component: Dashboard, 
   },
   {
-    path: '/welcome', // the url
-    component: Welcome, // view rendered
+    path: '/usermanagement', 
+    component: ManageUser, 
+  },
+  {
+    path: '/welcome', 
+    component: Welcome, 
   },
   {
     path: '/leads',
