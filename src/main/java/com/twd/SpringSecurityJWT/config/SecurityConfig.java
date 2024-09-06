c5b5b3f5867f767d363bb22341dcb887040c9e56
sovortey <126035238+sovortey514@users.noreply.github.com>
2024-09-06 09:17:25 +0700

    update image

--- src/main/java/com/twd/SpringSecurityJWT/config/SecurityConfig.java
+++ src/main/java/com/twd/SpringSecurityJWT/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(request -> request.requestMatchers("/auth/**", "/public/**").permitAll()
+                .requestMatchers("/admin/get_image/**").permitAll()
                         .requestMatchers("/admin/**").hasAnyAuthority("ADMIN")
                         .requestMatchers("/user/**").hasAnyAuthority("USER")
                         .requestMatchers("/adminuser/**").hasAnyAuthority("USER", "ADMIN")
